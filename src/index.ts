68f3a6a7da889d69dc585ba9712f08779edda0c5
David Sabine <david.sabine@live.com>
2025-08-22 17:04:13 -0600

    now fetching every minute

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+export default {
+	async fetch() {
+		return new Response('OK', { status: 200 })
+	},
+	async scheduled() {
+		console.log('Processing cron job…')
+		const response = await fetch('https://betterteamsacademy-nuxt4.david-f2e.workers.dev/api/scheduled-mail', {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+		const data = await response.json()
+		console.log(data)
+		return data
+	},
+}
